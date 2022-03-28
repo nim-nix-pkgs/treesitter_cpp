171f7b593f9cc8644365b167b852057c65c90f52
hugosenari <hugosenari@gmail.com>
2022-03-27 21:57:21 -0300

    chore: re index treesitter_cpp

--- master/flake.nix
+++ master/flake.nix
@@ -11,8 +11,8 @@
   inputs.src-treesitter_cpp-master.ref   = "refs/heads/master";
   inputs.src-treesitter_cpp-master.owner = "genotrance";
   inputs.src-treesitter_cpp-master.repo  = "nimtreesitter";
-  inputs.src-treesitter_cpp-master.dir   = "treesitter_cpp";
-  inputs.src-treesitter_cpp-master.type  = "github";
+    inputs.src-treesitter_cpp-master.dir   = "treesitter_cpp";
+inputs.src-treesitter_cpp-master.type  = "github";
   
   inputs."nimgen".owner = "nim-nix-pkgs";
   inputs."nimgen".ref   = "master";
